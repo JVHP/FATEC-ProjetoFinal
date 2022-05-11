9ad0ea2115f3d706178ace1175d54edf23140ee4
JVHP <trizede2010@hotmail.com>
2022-05-10 23:05:03 -0300

    <Fix> Tentativa de redirecionamento no login de http para https

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -35,7 +35,12 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if(env('APP_ENV') != 'local')
+            {
+                if ($e->getPrevious() instanceof \Illuminate\Session\TokenMismatchException) {
+                    return redirect()->route('login');
+                };
+            }
         });
     }
 }
